19a446f02a7117d891ad87478c6a54fa35ef6115
Oiz3e <izdidhawy@gmail.com>
2023-10-23 11:17:54 +0700

    fix menu

--- home.php
+++ home.php
@@ -32,6 +32,7 @@ include 'components/add_cart.php';
 
    <div class="carousel relative">
       <div class="carousel-inner">
+         
          <div class="carousel-item flex items-center justify-between"> 
             <div class="content text-white w-1/2 p-4"> 
                <h3 class="text-4xl font-bold text-center text-blue-500">Delicious Pizza</h3>
@@ -72,7 +73,7 @@ include 'components/add_cart.php';
    <div class="container mx-auto flex justify-between gap-4">
 
       <a href="category.php class="block"">
-         <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 hover:bg-blue-200">
+         <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 hover:bg-blue-200 transition-all duration-300">
             <img src="images/cat-1.png" alt="" class="w-20 mx-auto block">
             <a href="#">
                <h5 class="mb-2 text-center text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Makanan Cepat Saji</h5>
@@ -82,7 +83,7 @@ include 'components/add_cart.php';
       </a>
 
       <a href="category.php">
-         <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 hover:bg-blue-200">
+         <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 hover:bg-blue-200 transition-all duration-300">
             <img src="images/cat-2.png" alt="" class="w-20 mx-auto block">
             <a href="#">
                <h5 class="mb-2 text-center text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Hidangan Utama</h5>
@@ -92,7 +93,7 @@ include 'components/add_cart.php';
       </a>
 
       <a href="category.php">    
-         <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 hover:bg-blue-200">
+         <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 hover:bg-blue-200 transition-all duration-300">
             <img src="images/cat-3.png" alt="" class="w-20 mx-auto block">
             <a href="#">
                <h5 class="mb-2 text-center text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Minuman</h5>
@@ -102,7 +103,7 @@ include 'components/add_cart.php';
       </a>
 
       <a href="category.php">       
-         <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 hover:bg-blue-200">
+         <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 hover:bg-blue-200 transition-all duration-300">
             <img src="images/cat-4.png" alt="" class="w-20 mx-auto block">
             <a href="#">
                <h5 class="mb-2 text-center text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Makanan Penutup</h5>
@@ -114,9 +115,9 @@ include 'components/add_cart.php';
 </section>
 
 
-<section class="products text-center py-12 bg-white">
+<section class="products text-center mx-auto w-fit p-12 bg-white">
 
-   <h1 class="title text-3xl font-bold text-gray-800 mb-8">Hidangan Terbaru</h1>
+   <h1 class="title  text-3xl font-bold text-gray-800 mb-8">Hidangan Terbaru</h1>
 
    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
       
@@ -131,17 +132,17 @@ include 'components/add_cart.php';
          <input type="hidden" name="name" value="<?= $fetch_products['name']; ?>">
          <input type="hidden" name="price" value="<?= $fetch_products['price']; ?>">
          <input type="hidden" name="image" value="<?= $fetch_products['image']; ?>">
-         <a href="quick_view.php?pid=<?= $fetch_products['id']; ?>" class="fas fa-eye text-blue-600 text-xl"></a>
-         <button type="submit" class="fas fa-shopping-cart text-blue-600 text-xl" name="add_to_cart"></button>
-         <img src="uploaded_img/<?= $fetch_products['image']; ?>" alt="">
+         <div class="h-fit group mb-4">
+            <div class="relative overflow-hidden">
+               <!-- ini nanti fotonya ganti pake yang di database ya -->
+               <img src="images/dhawy.jpg" alt="">
+               <div class="absolute h-full w-full bg-black bg-opacity-0 flex items-center justify-center -bottom-0 group-hover:bg-opacity-20 opacity-0 group-hover:opacity-100 transition-all duration-300">
+                  <a href=""><button class="bg-black text-white p-2 px-5 font-semibold  rounded-full bg-blue-500">View details</button></a>
+               </div>
+            </div>
+         </div>
          <a href="category.php?category=<?= $fetch_products['category']; ?>" class="cat text-gray-600"><?= $fetch_products['category']; ?></a>
          <div class="name text-lg font-semibold text-gray-800 mt-2"><?= $fetch_products['name']; ?></div>
-         <div class="flex items-center mt-2">
-            <div class="price text-xl font-semibold text-gray-800">
-               <span class="text-gray-500">Rp.</span><?= $fetch_products['price']; ?>
-            </div>
-            <input type="number" name="qty" class="qty ml-4 border text-gray-800 items-center" min="1" max="99" value="1" maxlength="2">
-         </div>
       </form>
       <?php
             }
@@ -151,9 +152,7 @@ include 'components/add_cart.php';
       ?>
 
    </div>
-
-   <div class="more-btn mt-8">
-   <a href="menu.php" class="btn bg-transparent hover:bg-transparent text-blue-500 hover:text-blue-600 border-2 border-blue-500 hover:border-blue-600 text-white py-2 px-4 rounded-full">View All</a>
+   <a href="menu.php" ><button class="mt-9 -mb-2 font-semibold text-white text-sm text-center bg-blue-500 hover:bg-blue-700 rounded-full px-5 py-2.5">View All</button></a>
 </div>
 
 </section>
