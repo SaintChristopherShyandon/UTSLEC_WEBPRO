d5b925b56921b4b8426d301edc69e39bc9fa10e7
VerlyTan11 <verlytan11@gmail.com>
2023-10-23 20:47:53 +0700

    benerin detail menu

--- home.php
+++ home.php
@@ -14,6 +14,7 @@ include 'components/add_cart.php';
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -24,6 +25,7 @@ include 'components/add_cart.php';
     <link href="https://unpkg.com/tailwindcss@^2.2/dist/tailwind.min.css" rel="stylesheet">
 
 </head>
+
 <body class="bg-gray-50 font-sans">
 
     <?php include 'components/user_header.php'; ?>
@@ -33,42 +35,58 @@ include 'components/add_cart.php';
 
         <div class="container mx-auto flex justify-between gap-4">
             <a href="category.php" class="block">
-                <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 hover:bg-blue-200 transition-all duration-300">
+                <div
+                    class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 hover:bg-blue-200 transition-all duration-300">
                     <img src="images/cat-1.png" alt="" class="w-20 mx-auto block">
                     <a href="category.php">
-                        <h5 class="mb-2 text-center text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Makanan Cepat Saji</h5>
+                        <h5
+                            class="mb-2 text-center text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                            Makanan Cepat Saji</h5>
                     </a>
-                    <p class="mb-3 text-center font-normal text-gray-500 dark:text-gray-400">Makanan yang disajikan dan disiapkan dengan cepat dalam waktu yang singkat</p>
+                    <p class="mb-3 text-center font-normal text-gray-500 dark:text-gray-400">Makanan yang disajikan dan
+                        disiapkan dengan cepat dalam waktu yang singkat</p>
                 </div>
             </a>
 
             <a href="category.php">
-                <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 hover:bg-blue-200 transition-all duration-300">
+                <div
+                    class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 hover:bg-blue-200 transition-all duration-300">
                     <img src="images/cat-2.png" alt="" class="w-20 mx-auto block">
                     <a href="category.php">
-                        <h5 class="mb-2 text-center text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Hidangan Utama</h5>
+                        <h5
+                            class="mb-2 text-center text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                            Hidangan Utama</h5>
                     </a>
-                    <p class="mb-3 text-center font-normal text-gray-500 dark:text-gray-400">Hidangan yang biasanya menjadi fokus utama dalam suatu waktu makan</p>
+                    <p class="mb-3 text-center font-normal text-gray-500 dark:text-gray-400">Hidangan yang biasanya
+                        menjadi fokus utama dalam suatu waktu makan</p>
                 </div>
             </a>
 
             <a href="category.php">
-                <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 hover:bg-blue-200 transition-all duration-300">
+                <div
+                    class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 hover:bg-blue-200 transition-all duration-300">
                     <img src="images/cat-3.png" alt="" class="w-20 mx-auto block">
                     <a href="category.php">
-                        <h5 class="mb-2 text-center text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Minuman</h5>
+                        <h5
+                            class="mb-2 text-center text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                            Minuman</h5>
                     </a>
-                    <p class="mb-3 text-center font-normal text-gray-500 dark:text-gray-400">Segala jenis cairan yang dikonsumsi untuk menghilangkan dahaga, memberikan nutrisi, atau sekedar kenikmatan rasa</p>
+                    <p class="mb-3 text-center font-normal text-gray-500 dark:text-gray-400">Segala jenis cairan yang
+                        dikonsumsi untuk menghilangkan dahaga, memberikan nutrisi, atau sekedar kenikmatan rasa</p>
                 </div>
             </a>
 
             <a href="category.php">
-                <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 hover:bg-blue-200 transition-all duration-300">
+                <div
+                    class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 hover:bg-blue-200 transition-all duration-300">
                     <img src="images/cat-4.png" alt="" class="w-20 mx-auto block">
                     <a href="category.php">
-                        <h5 class="mb-2 text-center text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Makanan Penutup</h5>
+                        <h5
+                            class="mb-2 text-center text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                            Makanan Penutup</h5>
                     </a>
-                    <p class="mb-3 text-center font-normal text-gray-500 dark:text-gray-400">Hidangan yang disajikan setelah hidangan utama untuk menyegarkan mulut dan memberikan kenikmatan manis setelah makan</p>
+                    <p class="mb-3 text-center font-normal text-gray-500 dark:text-gray-400">Hidangan yang disajikan
+                        setelah hidangan utama untuk menyegarkan mulut dan memberikan kenikmatan manis setelah makan</p>
                 </div>
             </a>
         </div>
@@ -85,20 +103,21 @@ include 'components/add_cart.php';
             if ($select_products->rowCount() > 0) {
                 while ($fetch_products = $select_products->fetch(PDO::FETCH_ASSOC)) {
                     ?>
-                    <form action="" method="post" class="relative bg-white p-4 shadow-md rounded-md">
-                        <input type="hidden" name="pid" value="<?= $fetch_products['id']; ?>">
-                        <input type="hidden" name="name" value="<?= $fetch_products['name']; ?>">
-                        <input type="hidden" name="image" value="<?= $fetch_products['image']; ?>">
-                        <img src="resto-images/<?= $fetch_products['image']; ?>" alt="">
-                        <a href="category.php?category=<?= $fetch_products['category']; ?>" class="text-gray-600">
-                            <?= $fetch_products['category']; ?>
-                        </a>
-                        <div class="text-lg font-semibold text-gray-800 mt-2"><?= $fetch_products['name']; ?></div>
-                        <a href="menu.php" class="hover-button absolute inset-12 opacity-0 flex items-center justify-center text-white bg-blue-300 rounded-full transition-opacity duration-300 hover:opacity-100">
-                            View Details
-                        </a>
-                    </form>
-                    <?php
+            <form action="" method="post" class="relative bg-white p-4 shadow-md rounded-md">
+                <input type="hidden" name="pid" value="<?= $fetch_products['id']; ?>">
+                <input type="hidden" name="name" value="<?= $fetch_products['name']; ?>">
+                <input type="hidden" name="image" value="<?= $fetch_products['image']; ?>">
+                <img src="resto-images/<?= $fetch_products['image']; ?>" alt="">
+                <a href="category.php?category=<?= $fetch_products['category']; ?>" class="text-gray-600">
+                    <?= $fetch_products['category']; ?>
+                </a>
+                <div class="text-lg font-semibold text-gray-800 mt-2"><?= $fetch_products['name']; ?></div>
+                <a href="quick_view.php?pid=<?= $fetch_products['id']; ?>"
+                    class="hover-button absolute inset-12 opacity-0 flex items-center justify-center text-white bg-blue-300 rounded-full transition-opacity duration-300 hover:opacity-100">
+                    View Details
+                </a>
+            </form>
+            <?php
                 }
             } else {
                 echo '<p class="text-2xl text-gray-800">No products added yet!</p>';
@@ -110,29 +129,38 @@ include 'components/add_cart.php';
     <?php include 'components/footer.php'; ?>
 
     <script>
-        // List images dan title
-        const images = [
-            { src: "images/home-img-1.png", title: "Nasi Goreng Maknyus" },
-            { src: "images/home-img-2.png", title: "Es Podeng Seger" },
-            { src: "images/home-img-3.png", title: "Es Teh Manis" }
-        ];
-
-        // Function untuk update image dan title
-        function updateImage() {
-            const randomIndex = Math.floor(Math.random() * images.length);
-            const foodImage = document.getElementById("foodImage");
-            const foodTitle = document.getElementById("foodTitle");
-
-            foodImage.src = images[randomIndex].src;
-            foodTitle.textContent = images[randomIndex].title;
+    // List images dan title
+    const images = [{
+            src: "images/home-img-1.png",
+            title: "Nasi Goreng Maknyus"
+        },
+        {
+            src: "images/home-img-2.png",
+            title: "Es Podeng Seger"
+        },
+        {
+            src: "images/home-img-3.png",
+            title: "Es Teh Manis"
         }
+    ];
 
-        // Update image setiap 3 seconds (3000 milliseconds)
-        setInterval(updateImage, 3000);
+    // Function untuk update image dan title
+    function updateImage() {
+        const randomIndex = Math.floor(Math.random() * images.length);
+        const foodImage = document.getElementById("foodImage");
+        const foodTitle = document.getElementById("foodTitle");
 
-        // Initial image update
-        updateImage();
+        foodImage.src = images[randomIndex].src;
+        foodTitle.textContent = images[randomIndex].title;
+    }
+
+    // Update image setiap 3 seconds (3000 milliseconds)
+    setInterval(updateImage, 3000);
+
+    // Initial image update
+    updateImage();
     </script>
 
 </body>
+
 </html>
--- quick_view.php
+++ quick_view.php
@@ -16,25 +16,26 @@ include 'components/add_cart.php';
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-   <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>quick view</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>quick view</title>
 
-   <!-- Font Awesome CDN link -->
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
-   <link href="https://unpkg.com/tailwindcss@^2.2/dist/tailwind.min.css" rel="stylesheet">
+    <!-- Font Awesome CDN link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <link href="https://unpkg.com/tailwindcss@^2.2/dist/tailwind.min.css" rel="stylesheet">
 </head>
+
 <body>
-   
-<?php include 'components/user_header.php'; ?>
 
-<section class="quick-view bg-gray-100 p-12">
+    <?php include 'components/user_header.php'; ?>
 
-   <h1 class="text-3xl font-bold text-center mb-8">Quick View</h1>
+    <section class="quick-view bg-gray-100 p-12">
+        <h1 class="text-3xl font-bold text-center mb-8">Detail</h1>
 
-   <?php
+        <?php
       $pid = $_GET['pid'];
       $select_products = $conn->prepare("SELECT * FROM `products` WHERE id = ?");
       $select_products->execute([$pid]);
@@ -42,36 +43,45 @@ include 'components/add_cart.php';
          while($fetch_products = $select_products->fetch(PDO::FETCH_ASSOC)){
             $product_description = $fetch_products['deskripsi'];
    ?>
-   <form action="" method="post" class="box bg-white p-4 shadow-md rounded-md">
-      <input type="hidden" name="pid" value="<?= $fetch_products['id']; ?>">
-      <input type="hidden" name="name" value="<?= $fetch_products['name']; ?>">
-      <input type="hidden" name="price" value="<?= $fetch_products['price']; ?>">
-      <input type="hidden" name="image" value="<?= $fetch_products['image']; ?>">
-      <img src="resto-images/<?= $fetch_products['image']; ?>" alt="">
-      <a href="category.php?category=<?= $fetch_products['category']; ?>" class="cat"><?= $fetch_products['category']; ?></a>
-      <div class="name"><?= $fetch_products['name']; ?></div>
-      <p class="text-gray-600 mt-2"><?= $product_description ?></p>
-      <div class="flex justify-between items-center">
-         <div class="price text-2xl font-semibold"><span>Rp.</span><?= $fetch_products['price']; ?></div>
-         <input type="number" name="qty" class="qty w-16 p-2 border border-gray-300 rounded-md" min="1" max="99" value="1" maxlength="2">
-      </div>
-      <button type="submit" name="add_to_cart" class="cart-btn bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700 transition duration-300">Add to Cart</button>
-   </form>
-   <?php
+        <form action="" method="post" class="box bg-white p-4 shadow-md rounded-md">
+            <div class="flex justofy-center items-center">
+                <input type="hidden" name="pid" value="<?= $fetch_products['id']; ?>">
+                <input type="hidden" name="name" value="<?= $fetch_products['name']; ?>">
+                <input type="hidden" name="price" value="<?= $fetch_products['price']; ?>">
+                <input type="hidden" name="image" value="<?= $fetch_products['image']; ?>">
+                <img src="resto-images/<?= $fetch_products['image']; ?>" alt="" class="w-1/2">
+                <div class="flex flex-col justify-center ml-20">
+                    <a href="category.php?category=<?= $fetch_products['category']; ?>"
+                        class="justify-end flex"><?= $fetch_products['category']; ?></a>
+                    <div class="name text-2xl font-bold"><?= $fetch_products['name']; ?></div>
+                    <p class="text-gray-600 mt-2 mb-10"><?= $product_description ?></p>
+                    <div class="flex justify-between items-center mb-6">
+                        <div class="price text-2xl font-semibold"><span>Rp. </span><?= $fetch_products['price']; ?>,000
+                        </div>
+                        <input type="number" name="qty" class="qty w-16 p-2 border border-gray-300 rounded-md" min="1"
+                            max="99" value="1" maxlength="2">
+                    </div>
+                    <button type="submit" name="add_to_cart"
+                        class="cart-btn bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700 transition duration-300">Add
+                        to Cart</button>
+                </div>
+            </div>
+        </form>
+        <?php
          }
       }else{
          echo '<p class="empty text-center text-xl font-bold mt-8">No products added yet!</p>';
       }
    ?>
 
-</section>
+    </section>
 
-<?php include 'components/footer.php'; ?>
+    <?php include 'components/footer.php'; ?>
 
-<script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+    <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
 
-<!-- Custom JS file link -->
-<script src="js/script.js"></script>
+    <script src="js/script.js"></script>
 
 </body>
+
 </html>
